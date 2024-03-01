3aa2c394b648f97a3071181cb9ab46c35583d31b
Corey Wynn <corey.wynn@intent.ag>
2024-03-01 16:44:30 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const hitBackend = () => {
+    const url = "https://rjb792f1wi.execute-api.us-east-2.amazonaws.com/default/Python_API_test";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello</p>
+        <button onClick={() => hitBackend()}>Click me</button>
       </header>
     </div>
   );
