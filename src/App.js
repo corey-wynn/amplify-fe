8e533b63c2cdfa1179fb99b31166fcab92df6972
Corey Wynn <corey.wynn@intent.ag>
2024-03-21 16:38:51 -0500

    env variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 
 function App() {
+  // Load environment variables based on the environment
+  if (process.env.NODE_ENV === "local") {
+    // require("dotenv").config({ path: ".env" }); // Load .env for local development
+  } else {
+    // require("dotenv").config({ path: ".env.development" }); // Load .env.development for S3 deployment
+  }
+
   const hitBackend = () => {
     fetch(`${process.env.REACT_APP_API_URL}/route1`)
       .then((response) => {
