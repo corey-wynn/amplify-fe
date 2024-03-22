1af5acf653cc0bfac48c48340c735fe055713de6
Corey Wynn <corey.wynn@intent.ag>
2024-03-22 14:55:37 -0500

    add post method to route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [response, setResponse] = useState("");
   const hitBackend = () => {
     console.log("BAMF ---2---");
     console.log("BAMF - process.env", process.env);
-    fetch(`${process.env.REACT_APP_API_URL}/route1`)
+    fetch(`${process.env.REACT_APP_API_URL}/route1`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ key: "value" }) // replace with your data
+    })
       .then((response) => {
         const val = response.json();
-        console.log("BAMF - val", val);
         return val;
       })
-      .then((data) => console.log(data));
+      .then((data) => {
+        setResponse(data);
+      });
   };
 
   return (
     <div className="App">
       <header className="App-header">
+        {response?.message && <p>{response.message}</p>}
         <p>Hello</p>
         <button onClick={() => hitBackend()}>Click me</button>
       </header>
